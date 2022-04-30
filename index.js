ab565ddc94b3c43e14e05537756fdf5769f0f4a8
Artem <nimoshka@gmail.com>
2022-04-30 23:24:11 +0300

    Recommit last commit cause of git status issues

---
+++ index.js
@@ -0,0 +1,14 @@
+function computerPlay(){
+    let randomNumber = Math.floor(Math.random()*3) + 1;
+
+    switch(randomNumber){
+        case 1:
+            return "Rock";
+            break;
+        case 2:
+            return "Paper";
+            break;
+        case 3:
+            return "Scissors";
+    }
+}
